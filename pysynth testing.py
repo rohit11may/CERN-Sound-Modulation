fb6477ad20a4c96d0edbba87325d71b8f8bb17f8
Rohit <rohit11may@gmail.com>
2016-01-10 14:23:34 +0000

    stereo mixing of two wav files.

--- pysynth testing.py
+++ pysynth testing.py
@@ -1,3 +1,10 @@
 import pysynth
-test = ( ('c', 4), ('e', 4), ('g', 4), ('c5', 1))
-pysynth.make_wav(test, fn = "test.wav")
+from pysynth import mix_files
+song1 = (('c', 2), ('e', 4), ('g', 7), ('c5', 1))
+song2 = (('c5', 1),  ('g', 4), ('e', 4), ('c', 4))
+
+
+pysynth.make_wav(song1, fn = "test1.wav")
+pysynth.make_wav(song2, fn = "test2.wav")
+
+mix_files("test1.wav", "test2.wav", "test3.wav")
