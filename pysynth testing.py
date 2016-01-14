67e78bf1c7d3d1b8c561372b0138d70872d1e4f4
ap3006 <ashutoshpatra3006@gmail.com>
2016-01-14 15:04:40 +0200

    added notedict
    import notes in pysynth testing as well

--- pysynth testing.py
+++ pysynth testing.py
@@ -1,8 +1,10 @@
 from pysynth_src import pysynth
+from pysynth_src import notedict
 from pysynth_src.mixfiles import mix_files
 song1 = (('c', 2), ('e', 4), ('g', 7), ('c5', 1))
 song2 = (('c5', 1),  ('g', 4), ('e', 4), ('c', 4))
 
+print(notedict.notes)
 
 pysynth.make_wav(song1, fn = "test1.wav")
 pysynth.make_wav(song2, fn = "test2.wav")
